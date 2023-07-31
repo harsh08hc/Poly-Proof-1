f2a936e3b7852de51798b5b1f63e5c4a9b594699
harsh08hc <136369712+harsh08hc@users.noreply.github.com>
2023-08-01 01:08:33 +0530

    batchMint.js

--- scripts /batchMint.js
+++ scripts /batchMint.js
@@ -1,5 +1,3 @@
-// This script batch mints criceket ERC721A tokens.
-
 // Import required libraries
 const { ethers } = require("hardhat");
 require("dotenv").config();
@@ -10,7 +8,7 @@ async function main() {
 
   // The URL of the network provider
   const networkAddress =
-    "https://eth-goerli.g.alchemy.com/v2/h-asDNMJ21mVniDAY3XE1VQ9F7PW7A0x";
+    "https://eth-goerli.g.alchemy.com/v2/s_2SlXa-nrqnTKcKIE6GMlwkCmifw7Hh";
 
   // Create a provider using the URL
   const provider = new ethers.providers.JsonRpcProvider(networkAddress);
@@ -19,10 +17,10 @@ async function main() {
   const signer = new ethers.Wallet(privateKey, provider);
 
   // Tthe address of the deployed contract
-  const contractAddress = "0x032DB7b9DD6EB0cFFD06183515D77D5567E68037";
+  const contractAddress = "0x58fbb4B182d527AD3660E260d71Bd89E23f7dC5A";
 
   // Get the contract factory and attach it to the signer
-  const IndianNFT = await ethers.getContractFactory("cricketer", signer);
+  const IndianNFT = await ethers.getContractFactory("ronaldo", signer);
   const contract = await IndianNFT.attach(contractAddress);
 
   // Call the mint function on the contract to mint 5 tokens
